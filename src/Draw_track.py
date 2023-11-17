6791faa4fe3de8832cdb04c0153c2378c4d67f73
BenElliot <elliot.benj@gmail.com>
2023-11-17 16:06:31 +0000

    updates

--- src/Draw_track.py
+++ src/Draw_track.py
@@ -14,6 +14,8 @@ WALL_WIDTH = 0.5
 DISTANCE_BETWEEN_WALLS = 10
 
 RUBBER_SPRITE_IMG = '../images/rubber_sprite.png'
+RUBBER_INIT_SCALE = 0.05
+RUBBER_SCALING = 0.005
 
 class Draw_track(arcade.View):
 
@@ -23,6 +25,10 @@ class Draw_track(arcade.View):
         self.wall_list = arcade.SpriteList()
         self.wall_sprite = None
         self.draw_walls = False
+        self.erase_walls = False
+
+        self.rubber_sprite = Wall(RUBBER_SPRITE_IMG, scale=RUBBER_INIT_SCALE)
+        self.rubber_sprite.visible = False
 
         arcade.set_background_color(arcade.color.AMAZON)
 
@@ -34,6 +40,8 @@ class Draw_track(arcade.View):
         self.wall_list = arcade.SpriteList()
         self.wall_sprite = None
         self.draw_walls = False
+
+        self.rubber_sprite.visible = False
 
         arcade.set_background_color(arcade.color.AMAZON)
 
@@ -53,8 +61,18 @@ class Draw_track(arcade.View):
         arcade.draw_text("press S to save track", self.window.width - 90, self.window.height - 70,
                          arcade.color.WHITE, font_size=10, anchor_x="center")
 
+        if self.draw_walls:
+            arcade.draw_text(f"Mode: Drawing", 10, self.window.height - 30,
+                             arcade.color.WHITE, font_size=10, anchor_x="left")
+        elif self.erase_walls:
+            arcade.draw_text(f"Mode: Erasing", 10, self.window.height - 30,
+                             arcade.color.WHITE, font_size=10, anchor_x="left")
+            arcade.draw_text(f"Press UP/DOWN arrow keys to adjust eraser size", 10, self.window.height - 50,
+                             arcade.color.WHITE, font_size=10, anchor_x="left")
+
 
         self.wall_list.draw()
+        self.rubber_sprite.draw()
 
     def on_key_press(self, key, modifiers: int):
         """
@@ -66,19 +84,28 @@ class Draw_track(arcade.View):
 
         match key:
             case arcade.key.D:
-                self.draw_walls = True
-
-    def on_key_release(self, key, _modifiers: int):
-        """
-        Called whenever a key is released
-        :param key:
-        :param _modifiers:
-        :return:
-        """
-
-        match key:
-            case arcade.key.D:
+                # draw
+                self.erase_walls = False
+                self.rubber_sprite.visible = False
+                self.draw_walls = not self.draw_walls
+            case arcade.key.E:
+                # erase
                 self.draw_walls = False
+                self.rubber_sprite.visible = not self.rubber_sprite.visible
+                self.erase_walls = not self.erase_walls
+            case arcade.key.UP:
+                # make rubber bigger
+                self.rubber_sprite.scale += RUBBER_SCALING
+            case arcade.key.DOWN:
+                # make rubber smaller
+                self.rubber_sprite.scale -= RUBBER_SCALING
+            case arcade.key.S:
+                # save current track
+                # self.save_track(name='name')
+                pass
+
+
+
 
     def on_update(self, delta_time: float):
         pass
@@ -147,6 +174,23 @@ class Draw_track(arcade.View):
             if self.is_far_enough_from_other_wall(mouse_pos):
                 self.spawn_wall(mouse_pos)
 
+        elif self.erase_walls:
+            self.eraser(mouse_pos=[x, y])
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        """
+        Called every time mouse moves
+
+        :param int x: x position of mouse
+        :param int y: y position of mouse
+        :param int dx: Change in x since the last time this method was called
+        :param int dy: Change in y since the last time this method was called
+        """
+
+        if self.erase_walls:
+            self.rubber_sprite.position = (x, y)
+
+
     def save_track(self, name):
         """
         Saves the current track (by saving the positions of the walls as a pickle dump)
@@ -163,7 +207,7 @@ class Draw_track(arcade.View):
         pickle.dump([wall_positions, wall_angles])
         f.close()
 
-    def eraser(self):
+    def eraser(self, mouse_pos):
         """
         An erasor to erase current pieces of placed track
         Spawns a rubber sprite
@@ -171,6 +215,12 @@ class Draw_track(arcade.View):
         can make rubber bigger or lower with certain keys
         :return:
         """
+
+        self.rubber_sprite.position = tuple(mouse_pos)
+
+        delete_list = arcade.check_for_collision_with_list(self.rubber_sprite, self.wall_list)
+        for wall in delete_list:
+            self.wall_list.remove(wall)
 
 
 
